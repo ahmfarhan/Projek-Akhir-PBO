cc4ec5cadda57359aaeeca2cd7a326c56b01ec76
ahmfarhan <73058791+ahmfarhan@users.noreply.github.com>
2022-06-05 23:27:54 +0700

    Add files via upload

---
+++ Projek Akhir PBO/src/Main/SQLConnection.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Main;
+
+import java.sql.DriverManager;
+/**
+ * 
+ *
+ * @author inter
+ */ 
+public class SQLConnection {
+    private static java.sql.Connection koneksi;
+    
+    public static java.sql.Connection getKoneksi(){
+        if (koneksi == null){
+            try{
+                String url = "jdbc:mysql://localhost:3306/tugasakhir2";
+                String user = "root";
+                String password = "";
+                DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+                koneksi = DriverManager.getConnection(url, user, password);
+                System.out.println("Connection Successfully");
+            }catch (Exception e){
+                System.out.println("Error");
+            }
+        }
+        return koneksi;
+    }
+    
+    public static void main(String args[]){
+        getKoneksi();
+    }
+}
